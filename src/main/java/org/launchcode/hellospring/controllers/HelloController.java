368b1413cf44706a52096c91c1a0b5db2424d8f9
swankidelic <swankidelic@gmail.com>
2021-09-30 17:32:04 -0500

    completed exercise 10.5

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -6,35 +6,23 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    // handles requests for URI /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, HTML!";
-//    }
-
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, HTML!";
-    }
-
     // handler for /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public static String createMessage(@RequestParam String name, String lang) {
+        switch (lang) {
+            case "English": return "Hello, " + name + "!";
+            case "Badenglish": return "Go frick yerself, " + name + "!";
+            case "Senglish": return "Howdy, " + name + "!";
+            case "Spanglish": return "Oye, acho, you that " + name + "?";
+            case "Klingon": return "nuqneH, " + name + "!";
+            default: return "Who the heck are you??";
+        }
+
     }
 
-    // handler for /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello again, " + name + "!";
-    }
-
-    // lives at /hello/form
+    // handler for /form
     @GetMapping("form")
     public String helloForm(){
         return "<html>" +
@@ -44,8 +32,15 @@ public class HelloController {
                 "</title>" +
                 "</head>" +
                 "<body>" +
-                "<form action='hello' method='post'>" +
+                "<form action='hello' method='get'>" +
                 "<input type='text' name='name'>" + //submit a request to /hello
+                "<label for='lang'></label><select id='lang' name='lang'>" +
+                "<option value='English'>English</option>" +
+                "<option value='Badenglish'>Bad English</option>" +
+                "<option value='Senglish'>Southern English</option>" +
+                "<option value='Spanglish'>Spanglish</option>" +
+                "<option value='Klingon'>Klingon</option>" +
+                "</select>"+
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
