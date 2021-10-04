d9d223a3898c91ec7b890ebf6fdbb685c9802db1
swankidelic <swankidelic@gmail.com>
2021-10-03 22:43:11 -0500

    beginning Thymeleaf tutorial

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
 public class HelloController {
 
     // handler for /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
     public static String createMessage(@RequestParam String name, String lang) {
         switch (lang) {
             case "English": return "Hello, " + name + "!";
@@ -24,26 +24,7 @@ public class HelloController {
 
     // handler for /form
     @GetMapping("form")
-    public String helloForm(){
-        return "<html>" +
-                "<head>" +
-                "<title>" +
-                "helloForm" +
-                "</title>" +
-                "</head>" +
-                "<body>" +
-                "<form action='hello' method='get'>" +
-                "<input type='text' name='name'>" + //submit a request to /hello
-                "<label for='lang'></label><select id='lang' name='lang'>" +
-                "<option value='English'>English</option>" +
-                "<option value='Badenglish'>Bad English</option>" +
-                "<option value='Senglish'>Southern English</option>" +
-                "<option value='Spanglish'>Spanglish</option>" +
-                "<option value='Klingon'>Klingon</option>" +
-                "</select>"+
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    public String helloForm() {
+        return "form";
     }
 }
