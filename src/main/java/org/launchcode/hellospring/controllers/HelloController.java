9b546486db3de8ef093da7a215677a0819ac4ff7
swankidelic <swankidelic@gmail.com>
2021-10-04 20:33:10 -0500

    completed exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,29 +2,46 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     // handler for /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public static String createMessage(@RequestParam String name, String lang) {
+    public static String createMessage(@RequestParam String name, String lang, Model model) {
+        String greeting;
         switch (lang) {
-            case "English": return "Hello, " + name + "!";
-            case "Badenglish": return "Go frick yerself, " + name + "!";
-            case "Senglish": return "Howdy, " + name + "!";
-            case "Spanglish": return "Oye, acho, you that " + name + "?";
-            case "Klingon": return "nuqneH, " + name + "!";
-            default: return "Who the heck are you??";
+            case "English": greeting = "Hello, " + name + "!"; break;
+            case "Badenglish": greeting = "Go frick yerself, " + name + "!"; break;
+            case "Senglish": greeting = "Howdy, " + name + "!"; break;
+            case "Spanglish": greeting = "Oye, acho, you that " + name + "?"; break;
+            case "Klingon": greeting = "nuqneH, " + name + "!"; break;
+            default: greeting = "Who the heck are you??";
         }
-
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // handler for /form
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String hellNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Joel");
+        names.add("Crow");
+        names.add("Tom Servo");
+        names.add("Gypsy");
+        names.add("Cambot");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
